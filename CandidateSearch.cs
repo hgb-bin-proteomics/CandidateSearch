81451da7acab888d966b251e0dcf605f0d808ed4
Micha Birklbauer <micha.birklbauer@gmail.com>
2023-09-19 17:33:54 +0200

    add spectra reading functionality

--- CandidateSearch.cs
+++ CandidateSearch.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using CandidateSearch.util;
+
+namespace CandidateSearch
+{
+    public class CandidateSearch
+    {
+
+        public static void Main(string[] args)
+        {
+            if (args.Length == 3) { 
+                var spectra = MGFReader.readMGF(args[0]);
+                var database = args[1];
+                var settings = args[2];
+
+                return;
+            }
+
+            Console.WriteLine("Incorrect number of arguments! CandidateSearch needs exactly 3 arguments: spectra.mgf database.fasta settings.xml");
+            return;
+        }
+    }
+}
---
+++ util/Spectra.cs
@@ -0,0 +1,59 @@
+﻿namespace CandidateSearch.util
+{
+    public class Spectrum
+    {
+        public double[] mz;
+        public double[] intensity;
+        public int scanNumber;
+
+        public Spectrum(double[] Mz, double[] Intensity, int ScanNumber) 
+        {
+            mz = Mz;
+            intensity = Intensity;
+            scanNumber = ScanNumber;
+            Array.Sort(mz, intensity);
+        }
+
+        public int[] getEncoding(int massRange = 1300, int massMultiplier = 100)
+        {
+            var encoding = new List<int>();
+
+            for (int i = 0; i < mz.Length; i++)
+            {
+                if (mz[i] < massRange)
+                {
+                    encoding.Add((int) (mz[i] * massMultiplier));
+                }
+            }
+
+            return encoding.ToArray();
+        }
+    }
+
+    public static class MGFReader
+    {
+        public static List<Spectrum> readMGF(string filename)
+        { 
+            var MSAMANDA_spectra = MSAMANDA_MGFPARSER.MGFParser.ParseNextSpectra(filename);
+
+            var spectra = new List<Spectrum>();
+
+            foreach(var spectrum in MSAMANDA_spectra)
+            {
+                var mz = new List<double>();
+                var intensity = new List<double>();
+
+                foreach (var peak in spectrum.FragmentsPeaks)
+                {
+                    mz.Add(peak.Position);
+                    intensity.Add(peak.Intensity);
+                }
+
+                spectra.Add(new Spectrum(mz.ToArray(), intensity.ToArray(), spectrum.ScanNumber));
+            }
+
+            return spectra;
+        }
+    }
+}
+
