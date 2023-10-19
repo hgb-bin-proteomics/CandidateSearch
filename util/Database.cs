d3fd96ff1c343f7bd5f7eb2c768c68ca9a969ea0
Micha Birklbauer <micha.birklbauer@gmail.com>
2023-10-19 15:55:24 +0200

    add ion calculation

--- util/Database.cs
+++ util/Database.cs
@@ -1,23 +1,41 @@
-﻿namespace CandidateSearch.util
+﻿using System.Text;
+
+namespace CandidateSearch.util
 {
     public class Peptide
     {
         public string sequence;
+        public double mass;
         public Dictionary<int, double> modifications;
         public bool isDecoy;
+        public List<double> ions;
 
-        public Peptide(string Sequence, Dictionary<int, double> Modifications, bool IsDecoy)
+        // ion calculation parameters
+        public const int MAX_CHARGE = CandidateSearch.MAX_CHARGE;
+        public const int MAX_NEUTRAL_LOSSES = CandidateSearch.MAX_NEUTRAL_LOSSES;
+        public const int MAX_NEUTRAL_LOSS_MODS = CandidateSearch.MAX_NEUTRAL_LOSS_MODS;
+        public const string MAX_ALLOWED_CHARGE = CandidateSearch.MAX_ALLOWED_CHARGE;
+
+        public Peptide(string Sequence, double Mass, Dictionary<int, double> Modifications, bool IsDecoy)
         {
             sequence = Sequence;
+            mass = Mass;
             modifications = Modifications;
             isDecoy = IsDecoy;
+            ions = getIons(Sequence, Mass);
         }
 
         public int[] getEnconding(int massRange = 1300, int massMultiplier = 100)
         {
             var encoding = new List<int>();
 
-            // code
+            foreach (var ion in ions)
+            {
+                if (ion < massRange)
+                {
+                    encoding.Add((int) (ion * massMultiplier));
+                }
+            }
 
             return encoding.ToArray();
         }
@@ -36,6 +54,34 @@
             }
 
             return peptide + "]";
+        }
+
+        private List<double> getIons(string Sequence, double Mass)
+        {
+            var ions = new List<double>();
+
+            for (int charge = 1; charge <= MAX_CHARGE; charge++)
+            {
+                double[] outIonsNoNL;
+                MSAMANDA_IONCALCULATION.IonWithNL[] outIonsWithNL;
+                MSAMANDA_IONCALCULATION.Modification[] mods = new MSAMANDA_IONCALCULATION.Modification[sequence.Length + 2];
+                MSAMANDA_IONCALCULATION.IonCalculator.CalculateIons(out outIonsNoNL, 
+                                                                    out outIonsWithNL, 
+                                                                    Encoding.ASCII.GetBytes(Sequence),
+                                                                    Mass,
+                                                                    charge,
+                                                                    mods,
+                                                                    MAX_NEUTRAL_LOSSES,
+                                                                    MAX_NEUTRAL_LOSS_MODS,
+                                                                    0,
+                                                                    1300,
+                                                                    true,
+                                                                    MAX_ALLOWED_CHARGE);
+
+                ions.AddRange(outIonsNoNL);
+            }
+
+            return ions.Distinct().OrderBy(x => x).ToList();
         }
     }
 
