cfac5c6476171bc68e69b2024876f0ef8db97e08
Micha Birklbauer <micha.birklbauer@gmail.com>
2023-10-20 12:39:04 +0200

    fix TopN parameter

--- CandidateSearch.cs
+++ CandidateSearch.cs
@@ -106,7 +106,7 @@ namespace CandidateSearch
 
                 Console.WriteLine($"CPU search finished with code {memStat}. Search took {sw.Elapsed.TotalSeconds} seconds.");
 
-                var processedResult = new Result(ref result, ref peptides, ref spectra, TopN: 100);
+                var processedResult = new Result(ref result, ref peptides, ref spectra, TopN: TOP_N);
                 var csvStat = processedResult.export(spectraFile + "_results.csv");
 
                 Console.WriteLine($"Result file written to disk with code {csvStat}. Search finished!");
