0eb7a8b88a04705fd2674710f4b0760ea09e3fe9
Micha Birklbauer <micha.birklbauer@gmail.com>
2023-11-30 17:59:30 +0100

    update doxygen docs

--- docs/html/search/all_8.js
+++ docs/html/search/all_8.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['license_0',['License',['../md__r_e_a_d_m_e.html#autotoc_md7',1,'']]],
+  ['license_0',['License',['../md__r_e_a_d_m_e.html#autotoc_md8',1,'']]],
   ['limitations_1',['Limitations',['../md__r_e_a_d_m_e.html#autotoc_md4',1,'']]]
 ];
--- docs/html/search/all_c.js
+++ docs/html/search/all_c.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['result_0',['result',['../class_candidate_search_1_1util_1_1_result.html',1,'CandidateSearch.util.Result'],['../class_candidate_search_1_1util_1_1_result.html#a80d0c6d428f395b91034a3d4f14a42b9',1,'CandidateSearch.util.Result.result'],['../class_candidate_search_1_1util_1_1_result.html#a3629c97881b29058d9c800caa3f8e270',1,'CandidateSearch.util.Result.Result(ref int[] SearchResult, ref List&lt; Peptide &gt; Peptides, ref List&lt; Spectrum &gt; Spectra, int TopN)']]]
+  ['result_0',['result',['../class_candidate_search_1_1util_1_1_result.html',1,'CandidateSearch.util.Result'],['../class_candidate_search_1_1util_1_1_result.html#a80d0c6d428f395b91034a3d4f14a42b9',1,'CandidateSearch.util.Result.result'],['../class_candidate_search_1_1util_1_1_result.html#a3629c97881b29058d9c800caa3f8e270',1,'CandidateSearch.util.Result.Result(ref int[] SearchResult, ref List&lt; Peptide &gt; Peptides, ref List&lt; Spectrum &gt; Spectra, int TopN)']]],
+  ['results_1',['Results',['../md__r_e_a_d_m_e.html#autotoc_md5',1,'']]]
 ];
